a87f18829bc02c79e45fc19181c145e65af771d4
Ging.wu <wu.tongqing@gmail.com>
2018-06-12 18:27:12 +0800

    修改队列自动格式化数据

--- src/main/java/com/ipet/queue/redis/AbstractRedisQueueConsumer.java
+++ src/main/java/com/ipet/queue/redis/AbstractRedisQueueConsumer.java
@@ -8,6 +8,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisSentinelPool;
 
@@ -17,49 +20,49 @@ import java.util.concurrent.Executors;
 
 /**
  * @author 杨斌冰-工具组-技术中心
- *         <p>
- *         2018/3/1 15:31
+ * <p>
+ * 2018/3/1 15:31
  */
-public abstract class AbstractRedisQueueConsumer<T> implements IQueueConsumer {
+public abstract class AbstractRedisQueueConsumer implements IQueueConsumer {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    private JedisSentinelPool jedisSentinelPool;
+    private RedisConnectionFactory redisConnectionFactory;
 
     private String queueName;
 
-    public abstract void doConsume(T message);
+    public abstract void doConsume(String message);
 
     @Override
-    public void consume() throws Exception{
-        if(this.getClass().isAnnotationPresent(RedisQueueConsumer.class)){
+    public void consume() throws Exception {
+        if (this.getClass().isAnnotationPresent(RedisQueueConsumer.class)) {
             RedisQueueConsumer redisQueueConsumer = this.getClass().getAnnotation(RedisQueueConsumer.class);
             queueName = StringUtils.isNotBlank(redisQueueConsumer.value()) ? redisQueueConsumer.value() : redisQueueConsumer.queueName();
-            if(StringUtils.isBlank(queueName)){
+            if (StringUtils.isBlank(queueName)) {
                 throw new Exception("Redis Queue Consumer Java Config Error.");
             }
-            try(Jedis jedis = jedisSentinelPool.getResource()){
-                while (true){
-                    List<String> message = jedis.blpop(0,queueName);
-                    if(message != null && message.size() > 1){
-                        this.doConsume(JSON.parseObject(message.get(1),new TypeReference<T>(){}));
-                    }
+            RedisConnection connection = redisConnectionFactory.getConnection();
+            while (true) {
+                List<byte[]> message = connection.bLPop(0, queueName.getBytes("UTF-8"));
+                if (message != null && message.size() > 1) {
+                    String item = new String(message.get(1));
+                    this.doConsume(item);
                 }
             }
-        }else{
+        } else {
             throw new Exception("Redis Queue Consumer Java Config Error.");
         }
     }
 
     @PostConstruct
-    public void init(){
-        logger.info("The Consumer in Queue [{}] Started.",this.getClass().getName());
-        Executors.newSingleThreadExecutor().submit(()->{
+    public void init() {
+        logger.info("The Consumer in Queue [{}] Started.", this.getClass().getName());
+        Executors.newSingleThreadExecutor().submit(() -> {
             try {
                 this.consume();
             } catch (Exception e) {
-                logger.error("The Consumer in Queue [" + this.queueName + "] Error.",e);
+                logger.error("The Consumer in Queue [" + this.queueName + "] Error.", e);
             }
         });
     }
--- src/main/java/com/ipet/queue/redis/reflect/RedisQueueHandler.java
+++ src/main/java/com/ipet/queue/redis/reflect/RedisQueueHandler.java
@@ -7,55 +7,58 @@ import com.ipet.queue.redis.annotation.RedisQueueProvider;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisSentinelPool;
 
 import java.lang.reflect.Method;
+import java.nio.charset.Charset;
 import java.util.concurrent.Executors;
 
 /**
  * @author 杨斌冰-工具组-技术中心
- *         <p>
- *         2018/3/1 14:22
+ * <p>
+ * 2018/3/1 14:22
  */
 public class RedisQueueHandler implements IHandler {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private JedisSentinelPool jedisSentinelPool;
+    private RedisConnectionFactory redisConnectionFactory;
+
 
     @Override
-    public Object run(Method method, Object[] args) throws Exception{
+    public Object run(Method method, Object[] args) throws Exception {
         long rId = 0;
-        if(method.getDeclaringClass().isAnnotationPresent(RedisQueue.class) && method.isAnnotationPresent(RedisQueueProvider.class)){
-            if(args == null || args.length != 1){
+        if (method.getDeclaringClass().isAnnotationPresent(RedisQueue.class) && method.isAnnotationPresent(RedisQueueProvider.class)) {
+            if (args == null || args.length != 1) {
                 throw new Exception("Redis Queue Provider Java Interface Defined Error.");
             }
             RedisQueueProvider redisQueueProvider = method.getDeclaredAnnotation(RedisQueueProvider.class);
             String queueName = StringUtils.isNotBlank(redisQueueProvider.value()) ? redisQueueProvider.value() : redisQueueProvider.queueName();
-            if(StringUtils.isBlank(queueName)){
+            if (StringUtils.isBlank(queueName)) {
                 throw new Exception("Redis Queue Provider Java Config Error,please Check your configuration");
             }
-            try(Jedis jedis = jedisSentinelPool.getResource()){
-                switch (redisQueueProvider.strategy()){
-                    case SYNC:
-                        try{
-                            rId = jedis.rpush(queueName, JSON.toJSONString(args[0]));
-                            logger.info("Sync Send Message [{}] To Queue [{}].",JSON.toJSONString(args[0]),queueName);
-                        }catch (Exception e){
-                            logger.error("Sync Send Message [" + JSON.toJSONString(args[0]) + "] To Queue [" + queueName + "] Error.",e);
+            RedisConnection connection = redisConnectionFactory.getConnection();
+            switch (redisQueueProvider.strategy()) {
+                case SYNC:
+                    try {
+                        rId = connection.rPush(queueName.getBytes("UTF-8"), JSON.toJSONString(args[0]).getBytes("UTF-8"));
+                        logger.info("Sync Send Message [{}] To Queue [{}].", JSON.toJSONString(args[0]), queueName);
+                    } catch (Exception e) {
+                        logger.error("Sync Send Message [" + JSON.toJSONString(args[0]) + "] To Queue [" + queueName + "] Error.", e);
+                    }
+                    return rId;
+                case ASYNC:
+                    Executors.newSingleThreadExecutor().submit(() -> {
+                        try {
+                            connection.rPush(queueName.getBytes("UTF-8"), JSON.toJSONString(args[0]).getBytes("UTF-8"));
+                            logger.info("Async Send Message [{}] To Queue [{}].", JSON.toJSONString(args[0]), queueName);
+                        } catch (Exception e) {
+                            logger.error("ASync Send Message [" + JSON.toJSONString(args[0]) + "] To Queue [" + queueName + "] Error.", e);
                         }
-                        return rId;
-                    case ASYNC:
-                        Executors.newSingleThreadExecutor().submit(()-> {
-                            try{
-                                jedis.rpush(queueName,JSON.toJSONString(args[0]));
-                                logger.info("Async Send Message [{}] To Queue [{}].",JSON.toJSONString(args[0]),queueName);
-                            }catch (Exception e){
-                                logger.error("ASync Send Message [" + JSON.toJSONString(args[0]) + "] To Queue [" + queueName + "] Error.",e);
-                            }
-                        });
-                        return 1;
-                }
+                    });
+                    return 1;
             }
         }
         return rId;
@@ -63,14 +66,14 @@ public class RedisQueueHandler implements IHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if(Object.class.equals(method.getDeclaringClass())){
-            return method.invoke(this,args);
-        }else{
-            return run(method,args);
+        if (Object.class.equals(method.getDeclaringClass())) {
+            return method.invoke(this, args);
+        } else {
+            return run(method, args);
         }
     }
 
-    public void setJedisSentinelPool(JedisSentinelPool jedisSentinelPool) {
-        this.jedisSentinelPool = jedisSentinelPool;
+    public void setRedisConnectionFactory(RedisConnectionFactory redisConnectionFactory) {
+        this.redisConnectionFactory = redisConnectionFactory;
     }
 }
